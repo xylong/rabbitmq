0f1ab91fddcfbd43b795182aefeef3632b4206e7
xyl <416319808@qq.com>
2023-08-16 00:11:41 +0800

    发送消息

--- main.go
+++ main.go
@@ -7,10 +7,35 @@ import (
 )
 
 func main() {
+	// 连接mq
 	dsn := fmt.Sprintf("amqp://%s:%s@%s:%d/", "root", "123456", "localhost", 5672)
 	conn, err := amqp.Dial(dsn)
 	if err != nil {
 		log.Fatal(err)
 	}
 	defer conn.Close()
+
+	// 获取channel
+	c, err := conn.Channel()
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer c.Close()
+
+	// 创建队列
+	q, err := c.QueueDeclare("test", false, false, false, false, nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// 发送
+	body := "Hello World!"
+	if err := c.Publish("", q.Name, false, false, amqp.Publishing{
+		ContentType: "text/plain",
+		Body:        []byte(body),
+	}); err != nil {
+		log.Fatal(err)
+	}
+
+	log.Printf(" [x] Sent %s\n", body)
 }
